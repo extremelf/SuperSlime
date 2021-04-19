0b62df6f7f852188c614d2de26fe7a11f4dd20f3
extremelf <luis.paulo.6.1.2001@gmail.com>
2021-04-19 22:11:36 +0100

    Novo template, menu temporário e organização de código

--- game.js
+++ game.js
@@ -71,12 +71,10 @@ window.onload = function(){
             }
         },
         scene:[
-            menuScene,
-            {
-            preload: preload,
-            create: create,
-            update: update
-        }]
+            preloadGame,
+            menuGame,
+            playGame
+        ]
     }
 
     game = new Phaser.Game(gameConfig);
@@ -84,171 +82,252 @@ window.onload = function(){
     window.focus();
 };
 
-
-function preload ()
-{
-    this.load.image("background", "assets/PNG/game_background_4/game_background_4.png");
-    this.load.image("ground", "assets/ground.png");
-    this.load.image("baliza","assets/baliza.png");
-    this.load.image("balizaCollider","assets/balizaCollider.png");
-    this.load.image("ball","assets/ball.png");
-    this.load.spritesheet("slime","assets/img.png",
-        {frameWidth: 32, frameHeight: 48}
-    );
-}
-
-function create ()
-{
-
-    background = this.add.sprite(0,0,"background");
-    background.setOrigin(0,0);
-    background.displayWidth = game.config.width+100;
-    background.displayHeight = game.config.height+100;
-
-    ground = this.physics.add.staticGroup();
-    ground.create(0,game.config.height,"ground").setScale(2).refreshBody();
-    ground.setOrigin(0,10);
-
-    baliza = this.add.sprite(95,game.config.height-120,"baliza").setScale(0.3);
-    baliza.flipX= true;
-
-    balizaCollider = this.physics.add.staticGroup();
-    balizaCollider.create(180,game.config.height-120,"balizaCollider").setScale(0.3).refreshBody();
-
-    baliza2 = this.add.sprite(game.config.width- 95,game.config.height-120,"baliza").setScale(0.3);
-
-    balizaCollider2 = this.physics.add.staticGroup();
-    balizaCollider2.create(game.config.width-180, game.config.height-120, "balizaCollider").setScale(0.3).refreshBody();
-
-
-    ball = this.physics.add.image(game.config.width/2,game.config.height/2,"ball").setScale(0.15);
-
-    ball.body.setCircle(375);
-
-    ball.body.setBounce(0.3);
-    ball.body.setCollideWorldBounds(true,1,1);
-
-    player = this.physics.add.sprite(100,450,"slime");
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-    player2 = this.physics.add.sprite(game.config.width-100,450,"slime");
-    player2.setBounce(0.2);
-    player2.setCollideWorldBounds(true);
-
-    score1Text = this.add.text(16,16,"Score: 0",{fontSize: "32px", fill:"#000"});
-    score2Text = this.add.text(game.config.width-200,16,"Score: 0",{fontSize: "32px", fill:"#000"});
-
-    this.physics.add.collider(ball,balizaCollider,goal2);
-    this.physics.add.collider(ball,balizaCollider2,goal1);
-
-   // this.physics.add.collider(ball,balizaCollider,goal2,endgame,null,this);
-   // this.physics.add.collider(ball,balizaCollider2,goal1,endgame,null,this);
-
-    this.physics.add.collider(ball,ground);
-    this.physics.add.collider(player,ball);
-    this.physics.add.collider(ball,baliza);
-    this.physics.add.collider(ball,baliza2);
-    this.physics.add.collider(player,ground);
-    this.physics.add.collider(player2,ground);
-    this.physics.add.collider(player2,ball);
-
-    this.anims.create({
-        key: "left",
-        frames: this.anims.generateFrameNumbers("slime", { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: "turn",
-        frames: [{key: "slime",frame: 4}],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: "right",
-        frames: this.anims.generateFrameNumbers("slime", {start: 5, end: 8}),
-        frameRate: 10,
-        repeat: -1
-    });
-    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-
-}
-
-function update ()
-{
-
-    console.log("updated...");
-
-
-    if (gameover){
-        return;
+class preloadGame extends Phaser.Scene{
+    constructor(){
+        super("PreloadGame");
     }
 
-    cursors = this.input.keyboard.createCursorKeys();
-    wasd = {
-        up: Phaser.Input.Keyboard.KeyCodes.W,
-        down: Phaser.Input.Keyboard.KeyCodes.S,
-        left: Phaser.Input.Keyboard.KeyCodes.A,
-        right: Phaser.Input.Keyboard.KeyCodes.D,
-    };
+    preload(){
 
-    if(cursors.left.isDown){
-        player.setVelocityX(-160);
-        player.anims.play("left");
-    }
+        this.load.image("background", "assets/PNG/game_background_4/game_background_4.png");
+        this.load.image("ground", "assets/ground.png");
+        this.load.image("baliza","assets/baliza.png");
+        this.load.image("balizaCollider","assets/balizaCollider.png");
+        this.load.image("ball","assets/ball.png");
+        this.load.spritesheet("slime","assets/img.png",
+            {frameWidth: 32, frameHeight: 48}
+        );
 
-    else if(cursors.right.isDown){
-        player.setVelocityX(160);
-        player.anims.play("right");
-    }
-    else{
-        player.setVelocityX(0);
-        player.anims.play("turn");
-    }
-    if(cursors.up.isDown && player.body.touching.down){
-        player.setVelocityY(-330);
-    }
-    if(keyA.isDown){
-        player2.setVelocityX(-160);
-        player2.anims.play("left");
+        this.load.image("playbutton","assets/playbutton.png"); //
+        this.load.image("title","assets/title.png"); //
+        this.load.image("tile","assets/tile.png"); //
     }
 
-    else if(keyD.isDown){
-        player2.setVelocityX(160);
-        player2.anims.play("right");
-    }
-    else{
-        player2.setVelocityX(0);
-        player2.anims.play("turn");
-    }
-    if (keyW.isDown && player2.body.touching.down){
-        player2.setVelocityY(-330);
+    create(){
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("slime", { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{key: "slime",frame: 4}],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("slime", {start: 5, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.scene.start("MenuGame");
     }
 }
 
-function goal1(){
-    score1 +=1;
-    ball.setPosition(0,0);
-    score1Text.setText("Score: " + score1);
-}
+class menuGame extends Phaser.Scene{
 
-function goal2(){
-    score2 +=1;
-    ball.setPosition(0,0);
-    score2Text.setText("Score: " + score2);
-}
+    constructor(){
+        super("MenuGame");
+    }
 
-function endgame(score1, score2){
+    create(){
+        this.addGameTitle();
+    }
 
-    if (score1==60 || score2==60){
 
-        gameover=true
-        this.scene.restart();
+    addGameTitle(){
+        this.guiGroup = this.add.group();
 
+        let blackOverlay = this.add.sprite(0, 0, "tile");
+
+        blackOverlay.setOrigin(0, 0);
+        blackOverlay.displayWidth = game.config.width;
+        blackOverlay.displayHeight = game.config.height;
+        blackOverlay.alpha = 0.8;
+
+        this.guiGroup.add(blackOverlay);
+
+        let title = this.add.sprite(game.config.width / 2, 50, "title");
+        title.setOrigin(0.5, 0);
+
+        this.guiGroup.add(title);
+
+        let playButtonX = game.config.width / 2;
+        let playButtonY = game.config.height / 2 - 20;
+        let playButton = this.add.sprite( playButtonX, playButtonY, "playbutton");
+
+        playButton.setInteractive();
+
+        playButton.on("pointerup", function(){
+            this.guiGroup.toggleVisible();
+            this.guiGroup.active = false;
+            this.scene.start("PlayGame");
+        }, this);
+
+        this.guiGroup.add(playButton);
     }
 }
+
+class playGame extends Phaser.Scene{
+    constructor(){
+        super("PlayGame");
+    }
+
+    create(){
+        this.addBackground();
+        this.addPlayers();
+        this.addGameObjects();
+        this.addScore();
+        this.addColliders();
+        this.addWASD();
+    }
+
+    addBackground(){
+        background = this.add.sprite(0,0,"background");
+        background.setOrigin(0,0);
+        background.displayWidth = game.config.width+100;
+        background.displayHeight = game.config.height+100;
+
+        ground = this.physics.add.staticGroup();
+        ground.create(0,game.config.height,"ground").setScale(2).refreshBody();
+        ground.setOrigin(0,10);
+
+        balizaCollider = this.physics.add.staticGroup();
+        balizaCollider.create(180,game.config.height-120,"balizaCollider").setScale(0.3).refreshBody();
+
+
+        balizaCollider2 = this.physics.add.staticGroup();
+        balizaCollider2.create(game.config.width-180, game.config.height-120, "balizaCollider").setScale(0.3).refreshBody();
+
+
+    }
+
+    addPlayers(){
+        player = this.physics.add.sprite(100,450,"slime");
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        player2 = this.physics.add.sprite(game.config.width-100,450,"slime");
+        player2.setBounce(0.2);
+        player2.setCollideWorldBounds(true);
+    }
+
+    addGameObjects(){
+        baliza = this.add.sprite(95,game.config.height-120,"baliza").setScale(0.3);
+        baliza.flipX= true;
+
+        baliza2 = this.add.sprite(game.config.width- 95,game.config.height-120,"baliza").setScale(0.3);
+
+        ball = this.physics.add.image(game.config.width/2,game.config.height/2,"ball").setScale(0.15);
+
+        ball.body.setCircle(375);
+
+        ball.body.setBounce(0.3);
+        ball.body.setCollideWorldBounds(true,1,1);
+
+    }
+
+    addScore(){
+        score1Text = this.add.text(16,16,"Score: 0",{fontSize: "32px", fill:"#000"});
+        score2Text = this.add.text(game.config.width-200,16,"Score: 0",{fontSize: "32px", fill:"#000"});
+
+    }
+
+    addColliders(){
+        this.physics.add.collider(ball,balizaCollider,this.goal2);
+        this.physics.add.collider(ball,balizaCollider2,this.goal1);
+
+        // this.physics.add.collider(ball,balizaCollider,goal2,endgame,null,this);
+        // this.physics.add.collider(ball,balizaCollider2,goal1,endgame,null,this);
+
+        this.physics.add.collider(ball,ground);
+        this.physics.add.collider(player,ball);
+        this.physics.add.collider(ball,baliza);
+        this.physics.add.collider(ball,baliza2);
+        this.physics.add.collider(player,ground);
+        this.physics.add.collider(player2,ground);
+        this.physics.add.collider(player2,ball);
+    }
+
+    addWASD(){
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    }
+
+    update(){
+
+        console.log("updated...");
+
+        cursors = this.input.keyboard.createCursorKeys();
+        wasd = {
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D,
+        };
+
+        if(cursors.left.isDown){
+            player.setVelocityX(-160);
+            player.anims.play("left");
+        }
+
+        else if(cursors.right.isDown){
+            player.setVelocityX(160);
+            player.anims.play("right");
+        }
+        else{
+            player.setVelocityX(0);
+            player.anims.play("turn");
+        }
+        if(cursors.up.isDown && player.body.touching.down){
+            player.setVelocityY(-330);
+        }
+        if(keyA.isDown){
+            player2.setVelocityX(-160);
+            player2.anims.play("left");
+        }
+
+        else if(keyD.isDown){
+            player2.setVelocityX(160);
+            player2.anims.play("right");
+        }
+        else{
+            player2.setVelocityX(0);
+            player2.anims.play("turn");
+        }
+        if (keyW.isDown && player2.body.touching.down){
+            player2.setVelocityY(-330);
+        }
+    }
+
+    goal1(){
+        score1 +=1;
+        ball.setPosition(0,0);
+        score1Text.setText("Score: " + score1);
+    }
+
+    goal2(){
+        score2 +=1;
+        ball.setPosition(0,0);
+        score2Text.setText("Score: " + score2);
+    }
+
+    endgame(score1, score2){
+
+        if (score1===60 || score2===60){
+
+            gameover=true
+            this.scene.restart();
+
+        }
+    }
+}
+
+
+
